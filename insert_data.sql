ff3692740ce53f5164ec322b9dfbbc4c58a01aee
ChristianCurzi <91216278+ChristianCurzi@users.noreply.github.com>
2023-04-18 15:37:56 +0200

    Update

---
+++ insert_data.sql
@@ -0,0 +1,4 @@
+use TWeb;
+
+insert into Utente (Username, Password, Livello, Nome, Cognome, Email, DataNascita, Genere, Telefono)
+    VALUES ('ChriCurz','Belletette','3','Christian','Curzi','chricurz@belletette.com','2002-03-09','Maschio','8377373773');
